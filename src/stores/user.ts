0f53e9839712b5df50eb9249097fed36289d5d7d
Gael Grosset <gael@MacBook-Pro-de-Gael.local>
2025-12-09 09:57:01 +0100

    FIX - auth url

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -138,7 +138,7 @@ export default defineStore('user', {
 
     async register(data: object) {
       this.loading++
-      const url = import.meta.env.VITE_API_URL + 'Auth/Register'
+      const url = import.meta.env.VITE_API_URL + 'auth/register'
       try {
         const config = {
           url,
@@ -157,7 +157,7 @@ export default defineStore('user', {
 
     async verifyEmail(data: object) {
       this.loading++
-      const url = import.meta.env.VITE_API_URL + 'Auth/2fa/Verify'
+      const url = import.meta.env.VITE_API_URL + 'auth/2fa/verify'
       try {
         const config = {
           url,
@@ -180,7 +180,7 @@ export default defineStore('user', {
 
     async login(data: object) {
       this.loading++
-      const url = import.meta.env.VITE_API_URL + 'Auth/Login'
+      const url = import.meta.env.VITE_API_URL + 'auth/login'
       try {
         const config = {
           url,
@@ -199,7 +199,7 @@ export default defineStore('user', {
 
     async googleLogin(token: string) {
       this.loading++
-      const url = import.meta.env.VITE_API_URL + 'Auth/Google'
+      const url = import.meta.env.VITE_API_URL + 'auth/google'
       try {
         const config = {
           url,
